6e87a22b84ef0d8bf4b8ab331ceeb999e16a40a5
RyanBoyes <ryanboyes04@gmail.com>
2020-08-09 11:25:46 -0700

    y
    y

--- src/_01_Crazy_Digital_Painting/CrazyDigitalPainting.java
+++ src/_01_Crazy_Digital_Painting/CrazyDigitalPainting.java
@@ -1,27 +1,43 @@
 package _01_Crazy_Digital_Painting;
 
+import java.awt.Color;
+import java.util.Random;
+
 public class CrazyDigitalPainting {
 	//1. Create two final static integers for the width and height of the display.
-
+	final static int width = 1300;
+	final static int height = 800;
 	
 	//2. Create a 2D array of Color objects. You will need to import
 	//java.awt.Color. Initialize the size of the array using the 
 	//integers created in step 1.
-	
+	Color[][] c = new Color[1300][800];
 	
 	
 	public CrazyDigitalPainting() {
 		//3. Open the crazy_digital_painting.png file and look at the image.
-		
+		Random r1 = new Random();
+		Random r2 = new Random();
+		Random r3 = new Random();
+		int ran1 = r1.nextInt(256);
+		int ran2 = r2.nextInt(256);
+		int ran3 = r3.nextInt(256);
 		//4. Iterate through the 2D array and initialize each Color object
 		//   to a new color. The sample image was created using the following 
 		//   pattern:
 		//   colors[i][j] = new Color(i % 256, (i * j) % 256, j % 256);
+		for(int i = 0; i<c.length; i++) {
+			for(int j=0; j<c[i].length; j++) {
+				c[i][j] = new Color(i%ran1, (i*j)%ran2, j&ran3);
+				
+			}
+		}
 		
 		//5. Come up with your own pattern to make a cool crazy image.
 		
 		//6. Use the ColorArrayDisplayer class to call the displayColorsAsImage method 
 		//   to show off your picture.
+		ColorArrayDisplayer.displayColorsAsImage(c);
 	}
 	
 	public static void main(String[] args) {
--- src/_03_Conways_Game_of_Life/WorldPanel.java
+++ src/_03_Conways_Game_of_Life/WorldPanel.java
@@ -19,7 +19,7 @@ public class WorldPanel extends JPanel implements MouseListener, ActionListener 
 	private Timer timer;
 	
 	//1. Create a 2D array of Cells. Do not initialize it.
-
+	Cell[][] c;
 	
 	
 	public WorldPanel(int w, int h, int cpr) {
@@ -29,24 +29,51 @@ public class WorldPanel extends JPanel implements MouseListener, ActionListener 
 		this.cellsPerRow = cpr;
 	
 		//2. Calculate the cell size.
+		int size1 = c.length;
+		int size2 = c[size1].length;
+		
 		
 		//3. Initialize the cell array to the appropriate size.
+		c = new Cell[size1][size2];
 		
 		//3. Iterate through the array and initialize each cell.
 		//   Don't forget to consider the cell's dimensions when 
 		//   passing in the location.
+		for (int i = 0; i < c.length; i++) {
+			for (int j = 0; j < c[i].length; j++) {
+				c[i][j] = new Cell(w, h, cpr);
+			}
+		}
 		
 	}
 	
 	public void randomizeCells() {
 		//4. Iterate through each cell and randomly set each
 		//   cell's isAlive memeber to true of false
+		for (int i = 0; i < c.length; i++) {
+			for (int j = 0; j < c[i].length; j++) {
+				Random r = new Random();
+				int ran = r.nextInt(2);
+				if(ran==0) {
+					c[i][j].isAlive = true;
+				}
+				else if(ran==1) {
+					c[i][j].isAlive = false;
+				}
+			}
+		}
+		
 		
 		repaint();
 	}
 	
 	public void clearCells() {
 		//5. Iterate through the cells and set them all to dead.
+		for (int i = 0; i < c.length; i++) {
+			for (int j = 0; j < c[i].length; j++) {
+				c[i][j].isAlive = false;
+			}
+		}
 		
 		repaint();
 	}
@@ -66,7 +93,11 @@ public class WorldPanel extends JPanel implements MouseListener, ActionListener 
 	@Override
 	public void paintComponent(Graphics g) {
 		//6. Iterate through the cells and draw them all
-		
+		for (int i = 0; i < c.length; i++) {
+			for (int j = 0; j < c[i].length; j++) {
+				c[i][j].draw(g);
+			}
+		}
 		
 		
 		// draws grid
@@ -78,7 +109,13 @@ public class WorldPanel extends JPanel implements MouseListener, ActionListener 
 	public void step() {
 		//7. iterate through cells and fill in the livingNeighbors array
 		// . using the getLivingNeighbors method.
+		
 		int[][] livingNeighbors = new int[cellsPerRow][cellsPerRow];
+	for (int i = 0; i < livingNeighbors.length; i++) {
+		for (int j = 0; j < livingNeighbors[i].length; j++) {
+			getLivingNeighbors(i,j);
+		}
+	}
 		
 		//8. check if each cell should live or die
 	
