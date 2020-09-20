88f799e0343fa41f76a46464f85e70d1862084d1
RyanBoyes <ryanboyes04@gmail.com>
2020-09-20 11:21:38 -0700

    c
    c

--- src/extras/the_wrong_way_cow/TheWrongWayCow.java
+++ src/extras/the_wrong_way_cow/TheWrongWayCow.java
@@ -46,24 +46,81 @@ public class TheWrongWayCow {
     public static int[] findWrongWayCow(final char[][] field) {
         // Fill in the code to return the x,y coordinate position of the
         // head (letter 'c') of the wrong way cow!
-    	int x;
-    	int y;
+   
     	
-    	int[] wwc;
+    	int[] wwcR =  new int[2];
+    	int[] wwcL  = new int[2];
+    	int[] wwcU = new  int[2];
+    	int[] wwcD = new int[2];
+    	
+    	int upCow = 0;
+    	int rightCow = 0;
+    	int downCow = 0;
+    	int leftCow =  0;
+    	
+    	char cowStart;
     	
         
-    	if(field.))
+    	for (int y = 0; y < field.length; y++) {
+			for (int x = 0; x < field[y].length-3; x++) {
+				if(field[y][x]=='c' && field[y][x+1]=='o' && field[y][x+2]=='w') {
+					rightCow++;
+					wwcR[0] = x;
+					wwcR[1] = y;
+					
+			}
+		}
+    }		
+    		
+		
+    	for (int y = 0; y < field.length; y++) {
+			for (int x = 0; x < field[y].length-3; x++) {
+				if(field[y][x]=='w' && field[y][x+1]=='o' && field[y][x+2]=='c') {
+					leftCow++;
+					wwcL[0] = x;
+					wwcL[1] = y;
+			}
+		}
+    }
+    	
+    	for (int y = 0; y < field.length-3; y++) {
+			for (int x = 0; x < field[y].length; x++) {
+				if(field[y][x]=='c' && field[y+1][x]=='o' && field[y+2][x]=='w') {
+					downCow++;
+					wwcD[0] = x;
+					wwcD[1] = y;
+			}
+		}
+    }
+    	
+    	for (int y = 0; y < field.length; y++) {
+			for (int x = 0; x < field[y].length-3; x++) {
+				if(field[y][x]=='w' && field[y][x+1]=='o' && field[y][x+2]=='c') {
+					upCow++;
+					wwcU[0] = x;
+					wwcU[1] = y;
+			}
+		}
+    }
+    	
+    	if(rightCow==1) {
+    		return wwcR;
+    	}
+    	else if(leftCow==1) {
+    		return wwcL;
+    	}
+    	else if(upCow==1) {
+    		return wwcU;
+    	}
+    	else if(downCow==1) {
+    		return wwcD;
+    	}
+    	
+    	else {
+    		return null;
+    	}
     	
     	
-    	
-    	
-    	
-    	
-    	
-    	
-    	
-    	
-    	
-        return wwc;
+       
     }
 }
